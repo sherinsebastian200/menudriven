41e20ca2540ede17c62fb6896e286e28201ec4c2
sherin200 <sherinsebastian21>
2022-11-21 12:58:03 +0530

    update

--- src/MenuDriven.java
+++ src/MenuDriven.java
@@ -10,8 +10,7 @@ public class MenuDriven {
             int operation=sc.nextInt();
 
 
-            switch (operation)
-            {
+            switch (operation) {
                 case 1:
                     System.out.println("Enter the first number");
                     int a = sc.nextInt();
@@ -19,15 +18,13 @@ public class MenuDriven {
                     int b = sc.nextInt();
                     System.out.println("Enter the third number");
                     int c = sc.nextInt();
-                    if(a>b&&a>c){
-                        System.out.println(a+"is largesr");
-                    } else if (b>c)
-                    {
-                        System.out.println(b+"is largest");
+                    if (a > b && a > c) {
+                        System.out.println(a + "is largesr");
+                    } else if (b > c) {
+                        System.out.println(b + "is largest");
 
-                    }
-                    else {
-                        System.out.println(c+"is largset");
+                    } else {
+                        System.out.println(c + "is largset");
                     }
                     break;
                 case 2:
@@ -37,24 +34,33 @@ public class MenuDriven {
                     int y = sc.nextInt();
                     System.out.println("Enter the third number");
                     int z = sc.nextInt();
-                    if(x<y&&x<z){
-                        System.out.println(x+"is smallest");
-                    } else if (y<z)
-                    {
-                        System.out.println(y+"is smallest ");
+                    if (x < y && x < z) {
+                        System.out.println(x + "is smallest");
+                    } else if (y < z) {
+                        System.out.println(y + "is smallest ");
 
-                    }
-                    else {
-                        System.out.println(z+"is smallest");
+                    } else {
+                        System.out.println(z + "is smallest");
                     }
                     break;
 
-
+                case 3:
+                    System.out.println("enter the number for checking prime or not");
+                    int num = sc.nextInt();
+                    boolean flag = false;
+                    for (int i = 2; i <= num / 2; i++) {
+                        if (num % i == 0) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) {
+                        System.out.println(num + "is a prime number");
+                    } else {
+                        System.out.println(num + "is not a prime number");
+                    }
+                    break;
             }
-
-
-
-
 
             System.out.println("do you want to continue ?1.yes 2.no");
             ex=sc.nextInt();
