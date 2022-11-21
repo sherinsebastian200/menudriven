4b0b38898e281dc66e0f33d13bd88992e68a4a28
sherin200 <sherinsebastian21>
2022-11-21 12:30:56 +0530

    second commit

--- src/MenuDriven.java
+++ src/MenuDriven.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class MenuDriven {
+    public static void main(String[] args) {
+        int ex;
+        do{
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter 1.Largest of 3. 2.Smallest of 3 3.Prime or not 4.Even or not 5.Reverse 6.Exit");
+            int operation=sc.nextInt();
+
+
+            switch (operation) {
+
+            }
+
+
+
+
+
+            System.out.println("do you want to continue ?1.yes 2.no");
+            ex=sc.nextInt();
+
+
+
+
+        }while (ex==1);
+
+    }
 }
